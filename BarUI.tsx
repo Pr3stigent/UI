fb4b9305759bdc8e727f2da2620dff38270a1c41
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2021-08-02 16:23:09 -0400

    Update BarUI.tsx

--- BarUI.tsx
+++ BarUI.tsx
@@ -1,16 +1,12 @@
-// SERVICES
-import { Players, TweenService } from "@rbxts/services";
+import { Players } from "@rbxts/services";
 
-// MODULES
 import Roact, { Binding } from "@rbxts/roact";
 import { Spring, SingleMotor } from "@rbxts/flipper";
 
-// VARIABLES
 const player = Players.LocalPlayer;
 const character = player.Character || player.CharacterAdded.Wait()[0];
 const humanoid = character.WaitForChild("Humanoid") as Humanoid;
 
-// FUNCTIONS
 function map(x: number, inMin: number, inMax: number, outMin: number, outMax: number) {
 	return ((outMax - outMin) * (x - inMin)) / (inMax - inMin) + outMin;
 }
@@ -20,79 +16,90 @@ function oneOverProgressFunction(progress: number) {
 	return oneOverProgress;
 }
 
-interface BarData {
+type BarData = {
 	Name: string;
-	Size: UDim2 | Binding<UDim2>;
-	Size2: UDim2 | Binding<UDim2>;
 	Position: UDim2 | Binding<UDim2>;
-	OverlaySize: UDim2 | Binding<UDim2>;
-	Overlay2Size: UDim2 | Binding<UDim2>;
-	Overlay2Color: Color3;
 	Image: string;
-}
+	SpringOptions?: {};
+	SpringOptions2?: {};
+	Changed: RBXScriptSignal;
+	MaxValue: number;
+	ClippingData: {
+		SizeX: number;
+		SizeY: number;
+	};
+	OverlayData: {
+		SizeX?: number;
+		SizeY: number;
+	};
+	Overlay2Color: Color3;
+};
 
-function Bar(Data: BarData) {
+function Bar(data: BarData) {
+	const motor = new SingleMotor(100);
+	const motor2 = new SingleMotor(100);
+
+	const [binding, updateBinding] = Roact.createBinding(motor.getValue());
+	const [binding2, updateBinding2] = Roact.createBinding(motor2.getValue());
+
+	motor.onStep(updateBinding);
+	motor2.onStep(updateBinding2);
+
+	data.Changed.Connect((value) => {
+		motor.setGoal(new Spring(value, data.SpringOptions));
+		print("e");
+		task.delay(2, () => {
+			print("e2");
+			return motor2.setGoal(new Spring(value, data.SpringOptions2));
+		});
+	});
+
 	return (
-		<Roact.Fragment>
+		<>
 			<frame
-				Key={Data.Name}
-				Position={Data.Position}
-				Size={Data.Size}
+				Key={data.Name}
+				Position={data.Position}
+				Size={binding.map((value) => {
+					return UDim2.fromScale((value / data.MaxValue) * data.ClippingData.SizeX, data.ClippingData.SizeY);
+				})}
 				BackgroundTransparency={1}
-				ClipsDescendants={true}
+				ClipsDescendants
 			>
 				<imagelabel
-					Key={Data.Name}
+					Key={data.Name}
 					Position={UDim2.fromScale(0, 0)}
-					Size={Data.OverlaySize}
-					Image={Data.Image}
+					Size={binding.map((value) => {
+						return UDim2.fromScale(oneOverProgressFunction(value / data.MaxValue), data.OverlayData.SizeY);
+					})}
+					Image={data.Image}
 					BackgroundTransparency={1}
 				/>
 			</frame>
 			<frame
-				Key={`Damage ${Data.Name}`}
-				Position={Data.Position}
-				Size={Data.Size2}
+				Key={`Damage ${data.Name}`}
+				Position={data.Position}
+				Size={binding2.map((value) => {
+					return UDim2.fromScale((value / data.MaxValue) * data.ClippingData.SizeX, data.ClippingData.SizeY);
+				})}
 				BackgroundTransparency={1}
-				ClipsDescendants={true}
+				ClipsDescendants
 			>
 				<imagelabel
-					Key={Data.Name}
+					Key={data.Name}
 					Position={UDim2.fromScale(0, 0)}
-					Size={Data.Overlay2Size}
-					Image={Data.Image}
+					Size={binding2.map((value) => {
+						return UDim2.fromScale(oneOverProgressFunction(value / data.MaxValue), data.OverlayData.SizeY);
+					})}
+					Image={data.Image}
 					BackgroundTransparency={1}
-					ImageColor3={Data.Overlay2Color}
-					ZIndex={0}
 				/>
 			</frame>
-		</Roact.Fragment>
+		</>
 	);
 }
 
 function Bars() {
-	const healthMotor = new SingleMotor(100);
-	const damageHealthMotor = new SingleMotor(100);
-
-	const [health, updateHealth] = Roact.createBinding(healthMotor.getValue());
-	const [damageHealth, updateDamageHealth] = Roact.createBinding(damageHealthMotor.getValue());
-
-	const maxHealth = 100;
-	healthMotor.onStep(updateHealth);
-	damageHealthMotor.onStep(updateDamageHealth);
-
-	humanoid.HealthChanged.Connect((newHealth) => {
-		healthMotor.setGoal(
-			new Spring(newHealth, {
-				frequency: 1,
-			}),
-		);
-		damageHealthMotor.setGoal(
-			new Spring(newHealth, {
-				frequency: 0.01,
-			}),
-		);
-	});
+	const MAX_HEALTH = 100;
 
 	return (
 		<screengui>
@@ -103,8 +110,8 @@ function Bars() {
 			>
 				<imagelabel
 					Key={"Background"}
-					Position={UDim2.fromScale(0.018, 0.071)}
-					Size={UDim2.fromScale(0.96, 0.857)}
+					Position={UDim2.fromScale(0.024, 0.071)}
+					Size={UDim2.fromScale(0.938, 0.857)}
 					Image={"rbxassetid://7179258838"}
 					BackgroundTransparency={1}
 					ZIndex={-1}
@@ -113,41 +120,17 @@ function Bars() {
 					Name={"Healthbar"}
 					Position={UDim2.fromScale(0.266, 0.286)}
 					Image={"rbxassetid://7192049692"}
-					Size={health.map((newHealth) => {
-						return UDim2.fromScale((newHealth / maxHealth) * 0.619, 0.159);
-					})}
-					Size2={health.map((newHealth) => {
-						return UDim2.fromScale((newHealth / maxHealth) * 0.619, 0.159);
-					})}
-					OverlaySize={health.map((newHealth) => {
-						return UDim2.fromScale(oneOverProgressFunction(newHealth / maxHealth), 1);
-					})}
-					Overlay2Size={health.map((newHealth) => {
-						return UDim2.fromScale(oneOverProgressFunction(newHealth / maxHealth), 1);
-					})}
+					Changed={humanoid.HealthChanged}
+					SpringOptions={{ frequency: 2, dampeningRatio: 1 }}
+					SpringOptions2={{ frequency: 1, dampeningRatio: 1 }}
+					MaxValue={MAX_HEALTH}
+					ClippingData={{ SizeX: 0.6, SizeY: 0.159 }}
+					OverlayData={{ SizeY: 1 }}
 					Overlay2Color={Color3.fromRGB(154, 0, 0)}
 				/>
 			</frame>
 		</screengui>
 	);
 }
-
-/*
-<Bar
-					Name={"Staminabar"}
-					Position={UDim2.fromScale(0.274, 0.468)}
-					Size={UDim2.fromScale(0.537, 0.127)}
-					//Position2={UDim2.fromScale(-0.472, -3.116)}
-					OverlaySize={UDim2.fromScale(1.789, 6.75)}
-					Image={"rbxassetid://7179258642"}
-				/>
-				<Bar
-					Name={"Expbar"}
-					Position={UDim2.fromScale(0.254, 0.683)}
-					Size={UDim2.fromScale(0.503, 0.119)}
-					//Position2={UDim2.fromScale(-0.47, -5.129)}
-					OverlaySize={UDim2.fromScale(1.91, 7.2)}
-					Image={"rbxassetid://7179258773"}
-				/>*/
 
 export = Bars;
