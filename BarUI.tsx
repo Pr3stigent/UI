5d8c9c60f913c2a1c75bd173d699a168a50ad66b
Pr3stigent <83563230+Pr3stigent@users.noreply.github.com>
2021-08-05 14:33:03 -0400

    Update BarUI.tsx

--- BarUI.tsx
+++ BarUI.tsx
@@ -1,136 +1,147 @@
-import { Players } from "@rbxts/services";
+import Roact from "@rbxts/roact";
+import RoactRodux, { connect } from "@rbxts/roact-rodux";
+import flipper, { SingleMotor, Spring } from "@rbxts/flipper";
 
-import Roact, { Binding } from "@rbxts/roact";
-import { Spring, SingleMotor } from "@rbxts/flipper";
+import Actions from "client/Roact/Rodux/Actions";
+import Store from "client/Roact/Rodux/StoreTypes";
 
-const player = Players.LocalPlayer;
-const character = player.Character || player.CharacterAdded.Wait()[0];
-const humanoid = character.WaitForChild("Humanoid") as Humanoid;
-
-function map(x: number, inMin: number, inMax: number, outMin: number, outMax: number) {
-	return ((outMax - outMin) * (x - inMin)) / (inMax - inMin) + outMin;
-}
-
-function oneOverProgressFunction(progress: number) {
-	const oneOverProgress = progress === 0 ? 0 : 1 / progress;
-	return oneOverProgress;
-}
-
-type BarData = {
-	Name: string;
-	Position: UDim2 | Binding<UDim2>;
-	Image: string;
-	SpringOptions?: {};
-	SpringOptions2?: {};
-	Changed: RBXScriptSignal;
-	MaxValue: number;
-	ClippingData: {
-		SizeX: number;
-		SizeY: number;
-	};
-	OverlayData: {
-		SizeX?: number;
-		SizeY: number;
-	};
-	Overlay2Color: Color3;
+type Props = {
+	myRegion?: string;
+	Font: Enum.Font;
+	Font2?: Enum.Font;
+	RegionColor?: Color3;
+	BackgroundColor?: Color3;
 };
 
-function Bar(data: BarData) {
-	const motor = new SingleMotor(100);
-	const motor2 = new SingleMotor(100);
+function RegionNotifier(props: Props) {
+	const [lastRegion, updateLastRegion] = Roact.createBinding("N/A");
 
-	const [binding, updateBinding] = Roact.createBinding(motor.getValue());
-	const [binding2, updateBinding2] = Roact.createBinding(motor2.getValue());
+	const frameMotor = new SingleMotor(0);
+	const [frameBinding, updateFrameBinding] = Roact.createBinding(frameMotor.getValue());
 
-	motor.onStep(updateBinding);
-	motor2.onStep(updateBinding2);
+	const regionMotor = new SingleMotor(1);
+	const [regionBinding, updateRegionBinding] = Roact.createBinding(regionMotor.getValue());
 
-	data.Changed.Connect((value) => {
-		motor.setGoal(new Spring(value, data.SpringOptions));
-		print("e");
-		task.delay(2, () => {
-			print("e2");
-			return motor2.setGoal(new Spring(value, data.SpringOptions2));
+	const descriptionMotor = new SingleMotor(-1);
+	const [descriptionBinding, updateDescriptionBinding] = Roact.createBinding(descriptionMotor.getValue());
+
+	frameMotor.onStep(updateFrameBinding);
+	regionMotor.onStep(updateRegionBinding);
+	descriptionMotor.onStep(updateDescriptionBinding);
+
+	if (props.myRegion !== lastRegion.getValue()) {
+		updateLastRegion(props.myRegion as string);
+		frameMotor.setGoal(
+			new Spring(1.5, {
+				frequency: 0.7,
+			}),
+		);
+
+		const connection = frameMotor.onComplete(() => {
+			regionMotor.setGoal(
+				new Spring(0, {
+					frequency: 0.7,
+				}),
+			);
+			connection.disconnect();
 		});
-	});
+
+		const connection2 = regionMotor.onComplete(() => {
+			descriptionMotor.setGoal(
+				new Spring(0, {
+					frequency: 0.5,
+				}),
+			);
+			task.wait(6);
+			descriptionMotor.setGoal(
+				new Spring(-1, {
+					frequency: 0.5,
+				}),
+			);
+			task.wait(1);
+			regionMotor.setGoal(
+				new Spring(1, {
+					frequency: 0.5,
+				}),
+			);
+			task.wait(1);
+			frameMotor.setGoal(
+				new Spring(0, {
+					frequency: 0.7,
+				}),
+			);
+
+			connection2.disconnect();
+		});
+	}
 
 	return (
-		<>
-			<frame
-				Key={data.Name}
-				Position={data.Position}
-				Size={binding.map((value) => {
-					return UDim2.fromScale((value / data.MaxValue) * data.ClippingData.SizeX, data.ClippingData.SizeY);
+		<frame
+			Key={"RegionNotifier"}
+			Position={UDim2.fromScale(0.402, 0.012)}
+			Size={UDim2.fromScale(0.195, 0.168)}
+			BackgroundTransparency={1}
+		>
+			<imagelabel
+				Key={"Middle"}
+				AnchorPoint={new Vector2(0.5, 0)}
+				Position={UDim2.fromScale(0.5, 0.5)}
+				Size={frameBinding.map((value) => {
+					return UDim2.fromScale(value, 0.02);
 				})}
+				BackgroundTransparency={1}
+				BackgroundColor3={Color3.fromRGB(255, 255, 255)}
+				BorderSizePixel={0}
+				Image={"rbxassetid://7198153486"}
+				ImageTransparency={0.2}
+			></imagelabel>
+			<frame
+				Key={"Region"}
+				Position={UDim2.fromScale(0.015, 0.2)}
+				Size={UDim2.fromScale(0.971, 0.3)}
 				BackgroundTransparency={1}
 				ClipsDescendants
 			>
-				<imagelabel
-					Key={data.Name}
-					Position={UDim2.fromScale(0, 0)}
-					Size={binding.map((value) => {
-						return UDim2.fromScale(oneOverProgressFunction(value / data.MaxValue), data.OverlayData.SizeY);
+				<textlabel
+					Position={regionBinding.map((value) => {
+						return UDim2.fromScale(0, value);
 					})}
-					Image={data.Image}
+					Size={UDim2.fromScale(1, 1)}
 					BackgroundTransparency={1}
-				/>
+					Font={props.Font || Enum.Font.SpecialElite}
+					Text={props.myRegion}
+					TextColor3={props.RegionColor || Color3.fromRGB(255, 255, 255)}
+					TextScaled
+					TextStrokeTransparency={0.9}
+				></textlabel>
 			</frame>
 			<frame
-				Key={`Damage ${data.Name}`}
-				Position={data.Position}
-				Size={binding2.map((value) => {
-					return UDim2.fromScale((value / data.MaxValue) * data.ClippingData.SizeX, data.ClippingData.SizeY);
-				})}
+				Key={"Description"}
+				Position={UDim2.fromScale(0.015, 0.54)}
+				Size={UDim2.fromScale(0.971, 0.25)}
 				BackgroundTransparency={1}
 				ClipsDescendants
 			>
-				<imagelabel
-					Key={data.Name}
-					Position={UDim2.fromScale(0, 0)}
-					Size={binding2.map((value) => {
-						return UDim2.fromScale(oneOverProgressFunction(value / data.MaxValue), data.OverlayData.SizeY);
+				<textlabel
+					Position={descriptionBinding.map((value) => {
+						return UDim2.fromScale(0, value);
 					})}
-					Image={data.Image}
+					Size={UDim2.fromScale(1, 0.767)}
 					BackgroundTransparency={1}
-				/>
+					Font={props.Font2 || Enum.Font.SourceSansItalic}
+					TextColor3={props.BackgroundColor || Color3.fromRGB(0, 0, 0)}
+					TextScaled
+					TextStrokeTransparency={0.9}
+				></textlabel>
 			</frame>
-		</>
+		</frame>
 	);
 }
 
-function Bars() {
-	const MAX_HEALTH = 100;
+const mapStateToProps = (state: Store, props: {}) => {
+	return {
+		myRegion: state.myRegion,
+	};
+};
 
-	return (
-		<screengui>
-			<frame
-				Size={UDim2.fromScale(0.335, 0.211)}
-				Position={UDim2.fromScale(0.006, 0.762)}
-				BackgroundTransparency={1}
-			>
-				<imagelabel
-					Key={"Background"}
-					Position={UDim2.fromScale(0.024, 0.071)}
-					Size={UDim2.fromScale(0.938, 0.857)}
-					Image={"rbxassetid://7179258838"}
-					BackgroundTransparency={1}
-					ZIndex={-1}
-				/>
-				<Bar
-					Name={"Healthbar"}
-					Position={UDim2.fromScale(0.266, 0.286)}
-					Image={"rbxassetid://7192049692"}
-					Changed={humanoid.HealthChanged}
-					SpringOptions={{ frequency: 2, dampeningRatio: 1 }}
-					SpringOptions2={{ frequency: 1, dampeningRatio: 1 }}
-					MaxValue={MAX_HEALTH}
-					ClippingData={{ SizeX: 0.6, SizeY: 0.159 }}
-					OverlayData={{ SizeY: 1 }}
-					Overlay2Color={Color3.fromRGB(154, 0, 0)}
-				/>
-			</frame>
-		</screengui>
-	);
-}
-
-export = Bars;
+export = RoactRodux.connect(mapStateToProps)(RegionNotifier);
